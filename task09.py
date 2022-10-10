2d0553793b22f6144966ab8fae887513da73f087
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-10-10 23:05:37 +0200

    Added task09.py

---
+++ task09.py
@@ -0,0 +1,8 @@
+"""
+Palindrome Number
+https://leetcode.com/problems/palindrome-number/
+"""
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]: return True
+        return False
