2af5f523e5d9022a919ba81b625a100f53bc9dae
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-10-12 18:52:28 +0200

    Added task976.py

---
+++ task976.py
@@ -0,0 +1,15 @@
+"""
+Largest Perimeter Triangle
+https://leetcode.com/problems/largest-perimeter-triangle/
+"""
+from typing import List
+
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for i in range(len(nums) - 2):
+            if nums[i + 1] + nums[i + 2] > nums[i]:
+                return nums[i] + nums[i + 1] + nums[i + 2]
+
+        return 0
