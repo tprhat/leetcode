f95e3e6bd8c1d4fc545ff6b59d4c280678e432c7
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-10-17 18:18:03 +0200

    14. Longest Common Prefix

---
+++ 0014-LongestCommonPrefix.py
@@ -0,0 +1,19 @@
+"""
+14. Longest Common Prefix
+https://leetcode.com/problems/longest-common-prefix/
+"""
+
+from typing import List
+
+
+# zip(*strs) Iterate over all words in parallel, producing tuples with a letter from each one.
+# It will stop iteration on the shortest word.
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = []
+        for i in zip(*strs):
+            s = set(i)
+            if len(s) == 1:
+                prefix.append(s.pop())
+        return "".join(prefix)
