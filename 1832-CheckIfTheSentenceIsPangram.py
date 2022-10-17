6e91506c17d0fb074f6771479527dc90e0b6f0fd
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-10-17 13:47:24 +0200

    1832. Check if the Sentence Is Pangram

---
+++ 1832-CheckIfTheSentenceIsPangram.py
@@ -0,0 +1,11 @@
+"""
+Check if the Sentence Is Pangram
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+"""
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        s = set(sentence)
+        if len(s) == 26:
+            return True
+        return False
