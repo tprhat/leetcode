23aca5fa9a155deff7e339642d40a7de47bd3f75
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-10-17 18:00:01 +0200

    13. Roman to Integer

---
+++ 0013-RomanToInteger.py
@@ -0,0 +1,21 @@
+"""
+13. Roman to Integer
+https://leetcode.com/problems/roman-to-integer/
+"""
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        s = s.replace('IV', 'IIII').replace('IX', 'VIIII').replace('XL', 'XXXX').replace('XC', 'LXXXX').replace(
+            'CD', 'CCCC').replace('CM', 'DCCCC')
+
+        return sum(map(lambda x: d[x], s))
