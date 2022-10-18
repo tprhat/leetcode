5b1477e3a61ddbd2487b5affbcbf07dbc58c8a84
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-10-18 22:35:36 +0200

    38. Count and Say

---
+++ 0038-CountAndSay.py
@@ -0,0 +1,27 @@
+"""
+38. Count and Say
+https://leetcode.com/problems/count-and-say/
+"""
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        x = self.countAndSay(n - 1)
+
+        s = ""
+        y = x[0]
+        cnt = 1
+        for i in range(1, len(x)):
+            if x[i] == y:
+                cnt += 1
+
+            else:
+                s += str(cnt)
+                s += str(y)
+                y = x[i]
+                cnt = 1
+        s += str(cnt)
+        s += str(y)
+        return s
