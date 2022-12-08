a317bdc55b090dd9002995a87239a274af25d871
Tomislav Prhat <tomislav.prhat@fer.hr>
2022-12-08 19:16:10 +0100

    added explanation for the loop

--- 0338-CountingBits.py
+++ 0338-CountingBits.py
@@ -9,6 +9,6 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         res = [0]
         while len(res) <= n:
-            res += [i + 1 for i in res]
+            res += [i + 1 for i in res]  # [0], [0, 0+1], [0, 1, 0+1, 1+1], [0, 1, 1, 2, 0+1, 1+1, 1+1, 2+1], ...
             # print(res)
         return res[:n + 1]
