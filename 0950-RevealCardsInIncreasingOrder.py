09e4cf055147fd59e328825b0ba5e4ce8ef29533
tprhat <tomislav.prhat@gmail.com>
2024-04-10 16:37:45 +0200

    950

---
+++ 0950-RevealCardsInIncreasingOrder.py
@@ -0,0 +1,10 @@
+from typing import List, Any
+from collections import deque
+
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> deque[Any]:
+        q = deque()
+        for card in reversed(sorted(deck)):
+            q.rotate()
+            q.appendleft(card)
+        return q
