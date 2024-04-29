91b9e482e92b13d87d212e8d7324bb1bb1110641
tprhat <tomislav.prhat@gmail.com>
2024-04-29 21:23:16 +0200

    Minimum number of XOR operations

---
+++ medium/2997-MinNumOfOperationsXOR.py
@@ -0,0 +1,11 @@
+from functools import reduce
+from typing import List
+
+
+# For each different bit between the bitwise XOR of elements of the original array and k
+# we have to flip exactly one bit of an element in nums to make that bit equal
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        res = reduce(lambda x, y: x ^ y, nums)
+        res = res ^ k
+        return bin(res).count('1')
