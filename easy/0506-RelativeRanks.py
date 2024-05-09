2e41e4d7f2e660432793e7b13cd6f896a97b4d89
tprhat <tomislav.prhat@gmail.com>
2024-05-09 15:59:31 +0200

    relative ranks

---
+++ easy/0506-RelativeRanks.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        placments = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
+        for i, x in enumerate(sorted(score, reverse=True)):
+            if i < 3:
+                score[score.index(x)] = placments[i]
+            else:
+                score[score.index(x)] = str(i + 1)
+
+        return score
